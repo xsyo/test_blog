3fe417c52a4069dc173c3d1bd5814a677e202e0b
xsyo <xsyu1324@mail.ru>
2020-08-27 22:42:29 +0600

    commit

--- conf/settings.py
+++ conf/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'test_blog',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgre',
-#         'HOST': 'localhost',
-#         'POST': '50079,'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_blog',
+        'USER': 'postgres',
+        'PASSWORD': 'postgre',
+        'HOST': 'localhost',
+        'POST': '50079,'
+    }
+}
 
 
 # Password validation
